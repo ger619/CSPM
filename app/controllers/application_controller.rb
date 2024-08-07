3234dfed5161a46c39d74e1d427c36fec9eefddb
ger619 <abolger254@gmail.com>
2024-08-07 17:48:34 +0300

    adding UI details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :current_password, :profile_picture)
+      u.permit(:name, :email, :password, :current_password, :profile_picture, :first_name, :last_name)
     end
   end
 end
