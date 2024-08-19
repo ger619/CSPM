b3e389f4006336fcecebeb1d6fa2e66770f27758
ger619 <abolger254@gmail.com>
2024-08-19 16:27:32 +0300

    On first sign in you have to ensure you update the user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,11 @@ class ApplicationController < ActionController::Base
 
   def check_profile_completion
     if user_signed_in? && !current_user.first_login && controller_name != 'registrations' && action_name != 'edit'
-      redirect_to edit_user_registration_path, alert: 'Please complete your profile before continuing.'
+      render edit_user_registration_path, alert: 'Please complete your profile before continuing.'
     end
     return unless user_signed_in? && controller_name == 'registrations' && action_name == 'update'
 
     current_user.update(first_login: true, first_name: params[:user][:first_name], last_name: params[:user][:last_name])
-    redirect_to project_index_path, alert: 'Please complete your profile before continuing.'
-
+    redirect_to root_path, alert: 'Profile is Updated.'
   end
 end
