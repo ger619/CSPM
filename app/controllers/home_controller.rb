3f5e61183f4fe7db4fa041105408f6221cbe259e
ger619 <draker74@gmail.com>
2024-09-07 02:57:23 +0300

    assigning a project for a user per project to add up all

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,18 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
     @users = User.all.order('created_at DESC')
-    @project_ticket_counts = current_user.projects.joins(:tickets).group('projects.title').count('tickets.id')
+
+    # Count the number of issues created by the current user per project
+    @issues_count_per_project_for_current_user = current_user.projects
+      .joins(tickets: :issues)
+      .where('issues.user_id = ?', current_user.id) # Only count issues by the current user
+      .group('projects.title')
+      .count('issues.id')
+
+    # Count the total number of tickets created for each project (by anyone)
+    @tickets_count_per_project = current_user.projects
+      .joins(:tickets)
+      .group('projects.title')
+      .count('tickets.id')
   end
 end
