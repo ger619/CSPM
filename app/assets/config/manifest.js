eb5354baa1a0339da5153a7790ff6d71b3e3a481
ger619 <abolger254@gmail.com>
2024-09-13 12:30:19 +0300

    Adding the pipeline asset to deploy

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,3 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../builds
-//= link_tree ../images
-//= link_directory ../images .png .jpg .jpeg .gif .svg .ico .webp .bmp .tiff
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
