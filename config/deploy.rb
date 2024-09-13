afd5ed5d9cefb7646689a4c82e8ffb9d01daab6e
ger619 <abolger254@gmail.com>
2024-09-13 16:34:30 +0300

    adding bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,17 +31,20 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-set :default_env, { path: "/home/cspm/.rbenv/shims:/home/cspm/.rbenv/bin:$PATH" }
-
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+set :default_env, { path: "/home/cspm/.rbenv/shims:/home/cspm/.rbenv/bin:$PATH" }
+
+
 namespace :deploy do
-  after :updated, 'deploy:compile_assets'
+  after :updated, 'deploy:compile_assets' do
+  end
 end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+
+  # Uncomment the following to require manually verifying the host key before first deploy.
+  # # set :ssh_options, verify_host_key: :secure
