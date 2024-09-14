725057b8aa00cff84733584b26afaffd18b385f6
ger619 <draker74@gmail.com>
2024-09-14 13:25:22 +0300

    adding tailwind

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage", "app/assets/images","app/assets/stylesheets", "app/assets/stylesheets/application.tailwind.css",
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage", "app/assets/images","app/assets/stylesheets", "app/assets/stylesheets/application.tailwind.css"
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
