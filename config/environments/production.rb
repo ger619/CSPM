e338d5eef86d893b9ad819ec435cd876c6477899
ger619 <abolger254@gmail.com>
2024-09-17 13:44:24 +0300

    remove port 3000

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'craftsilicon.com', protocol: 'http' }
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: '172.17.20.11', port: 3000 }
+  config.action_mailer.default_url_options = { host: '172.17.20.11'}
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
