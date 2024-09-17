46099965b06afc84598c48972b26c33ccfa9ce17
ger619 <abolger254@gmail.com>
2024-09-17 13:05:31 +0300

    add devise in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,26 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_mailer.default_url_options = { host: 'craftsilicon.com', protocol: 'http' }
+  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_mailer.default_url_options = { host: 'localhost' }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'secure.emailsrvr.com',
+    port: 465, # Use 587 for STARTTLS or 465 for SSL/TLS
+    domain: 'craftsilicon.com', # Replace with your domain
+    user_name: 'cspm@craftsilicon.com', # Replace with your email
+    password: '#cspm@123#', # Replace with your email password
+    authentication: 'plain', # Can also be 'plain' or 'cram_md5'
+    ssl: true, # Use SSL encryption
+    tls: true, # Enforce TLS
+    enable_starttls_auto: true, # Automatically start TLS if available
+    openssl_verify_mode: 'none' # To avoid certificate verification issues (use cautiously)
+  }
+
 end
