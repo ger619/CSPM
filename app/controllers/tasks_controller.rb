771b0717e7707b3275f109f60d1df9869d3ccf3b
ger619 <abolger254@gmail.com>
2024-09-27 09:46:50 +0300

    Merge pull request #95 from ger619/done-day
    App clean up and image set up changes clean up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,7 @@ class TasksController < ApplicationController
         format.html { redirect_to product_path(@product), notice: 'Task was successfully created.' }
       else
         format.html { redirect_to new_product_board_task_path(@product, @board), alert: 'Task was not created.' }
-        #format.html { render :new, alert: 'Task was not created.' }
+        # format.html { render :new, alert: 'Task was not created.' }
       end
     end
   end
