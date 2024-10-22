f5cf53e8037a7009849fccd16ac190f27e5d57c9
ger619 <abolger254@gmail.com>
2024-10-22 16:56:22 +0300

    Adding restrictions to the status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_22_072923) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_22_130012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_22_072923) do
     t.datetime "updated_at", null: false
     t.uuid "user_id", null: false
     t.uuid "project_id"
+    t.string "status"
     t.index ["project_id"], name: "index_comments_on_project_id"
     t.index ["ticket_id"], name: "index_comments_on_ticket_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
