5614d48cf8b8b67aee7f7410bfefdf8a47280a86
ger619 <abolger254@gmail.com>
2024-11-05 12:25:00 +0300

    Adding status to the project controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -130,27 +130,21 @@ class TicketsController < ApplicationController
   def add_status
     @project = Project.find(params[:project_id])
     @ticket = @project.tickets.find(params[:id])
-    @ticket.status = Status.find_by(params[:status_id])
 
-    Rails.logger.debug "Status ID: #{params[:status_id]}" # Debugging line
+    status = Status.find_by(params[:status_id])
+    @ticket.status_id = status
 
-    if params[:status_id].blank?
-      Rails.logger.debug "Status ID is missing link: #{params[:status_id]}"
-      return redirect_to project_ticket_path(@project, @ticket), alert: 'Status ID is missing'
-    end
-
-    status = Status.find(params[:status_id])
-
-    # Update the status of the ticket
-    @ticket.update(status:)
-
-    # Notify users about the status update
-    @ticket.users.each do |ticket_user|
-      UserMailer.status_update_email(ticket_user, @ticket, current_user).deliver_later
-    end
-
-    respond_to do |format|
-      format.html { redirect_to project_ticket_path(@project, @ticket), notice: 'Status updated successfully' }
+    if @ticket.save
+      @ticket.users.each do |ticket_user|
+        UserMailer.status_update_email(ticket_user, @ticket, current_user).deliver_later
+      end
+      respond_to do |format|
+        format.html { redirect_to project_ticket_path(@project, @ticket), notice: 'Status updated successfully' }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to project_ticket_path(@project, @ticket), alert: 'Failed to update status' }
+      end
     end
   end
 
@@ -166,6 +160,6 @@ class TicketsController < ApplicationController
 
   def ticket_params
     params.require(:ticket).permit(:issue, :priority, :content, :project_id, :user_id, :ticket_image,
-                                   :status, :status_id, user_ids: [])
+                                   :status_id, user_ids: [])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_31_091529) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_091607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -241,7 +241,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_31_091529) do
     t.uuid "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.datetime "initial_response_deadline"
     t.datetime "target_repair_deadline"
     t.datetime "resolution_deadline"
