db11872afccd34c327e2f4b2f8825ed1fd67be3e
ger619 <abolger254@gmail.com>
2024-11-08 17:22:43 +0300

    Adding SLA to tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -24,12 +24,48 @@ class Ticket < ApplicationRecord
 
   after_create :set_initial_response_time
   def set_initial_response_time
-    # return unless saved_change_to_status? && Ticket.statuses == 'assigned'
+    start_time = DateTime.now
+    start_time = adjust_start_time(start_time)
+    update_column(:initial_response_deadline, next_business_time(start_time, 30.minutes))
+  end
 
-    update_column(:initial_response_deadline, next_business_time(DateTime.now, 30.minutes))
+  def sla_status
+    # For Sla to be met, the ticket must change status to from status.nil? to status.present within the first 30 minutes
+    # Show the ticket status is not blank
+    return unless statuses.exists?
+
+    update_column(:status, 'true')
   end
 
   private
+
+  def adjust_start_time(start_time)
+    business_hours = [
+      { day: 1, start: 8, end: 13 }, { day: 1, start: 14, end: 17 },
+      { day: 2, start: 8, end: 13 }, { day: 2, start: 14, end: 17 },
+      { day: 3, start: 8, end: 13 }, { day: 3, start: 14, end: 17 },
+      { day: 4, start: 8, end: 13 }, { day: 4, start: 14, end: 17 },
+      { day: 5, start: 8, end: 13 }, { day: 5, start: 14, end: 17 },
+      { day: 6, start: 8, end: 13 }
+    ]
+
+    until within_business_hours?(start_time, business_hours)
+      start_time += 1.minute
+      # Skip time between 1pm and 2pm
+      if start_time.hour == 13
+        start_time = start_time.change(hour: 14, min: 0)
+      end
+      # Skip time after 5pm
+      if start_time.hour >= 17
+        start_time = start_time.change(hour: 8, min: 0) + 1.day
+      end
+      # Skip non-business days (Sunday)
+      if start_time.wday == 0
+        start_time = start_time.change(hour: 8, min: 0) + 1.day
+      end
+    end
+    start_time
+  end
 
   def next_business_time(start_time, duration)
     business_hours = [
@@ -42,7 +78,21 @@ class Ticket < ApplicationRecord
     ]
 
     end_time = start_time + duration
-    end_time += 1.minute until within_business_hours?(end_time, business_hours)
+    until within_business_hours?(end_time, business_hours)
+      end_time += 1.minute
+      # Skip time between 1pm and 2pm
+      if end_time.hour == 13
+        end_time = end_time.change(hour: 14, min: 0)
+      end
+      # Skip time after 5pm
+      if end_time.hour >= 17
+        end_time = end_time.change(hour: 8, min: 0) + 1.day
+      end
+      # Skip non-business days (Sunday)
+      if end_time.wday == 0
+        end_time = end_time.change(hour: 8, min: 0) + 1.day
+      end
+    end
     end_time
   end
 
