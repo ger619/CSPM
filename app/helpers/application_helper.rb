fe16e84b16a0b731d9c0440be705daf66895c472
Robert Kanyoro <rmjesusaddict@gmail.com>
2024-11-27 16:27:57 +0300

    Add the helper method to close the modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def cancel_redirect_path
+    referer = request.referer || root_path
+    referer.gsub(/\/comments\/new$/, '') # Removes '/comments/new' from the referer URL
+  end
 end
