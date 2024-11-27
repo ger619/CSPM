eac2dcdd2bdc1931dff3c936e366fa2a29f72555
Robert Kanyoro <rmjesusaddict@gmail.com>
2024-11-27 16:36:13 +0300

    Update ticket creation redirect and improve comment form UI
    Modify the redirect after ticket creation to the ticket's show page for better navigation. Enhance the comment form by simplifying the submit button label and adding Turbo support to prevent unwanted reloads. Ensure better user experience and consistency across the application.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -33,14 +33,16 @@ class TicketsController < ApplicationController
     respond_to do |format|
       if @tickets.save
         current_user.add_role :creator, @tickets
+
         # Update the status to "new"
         status = Status.find_by(name: 'New')
         @tickets.statuses << status if status
-        # Assign the user with the role of project manager to the ticket
 
-        format.html { redirect_to project_path(@project), notice: 'ticket was successfully created.' }
+        # Assign the user with the role of project manager to the ticket (if necessary logic applies here)
+
+        # Redirect to the ticket's show page
+        format.html { redirect_to project_ticket_path(@project, @tickets), notice: 'Ticket was successfully created.' }
       else
-        # redirect_to new_project_path(@project), alert: 'ticket was not created.'
         format.html { render :new, status: :unprocessable_entity }
       end
     end
