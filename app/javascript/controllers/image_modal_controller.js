733c16527ded73052811276223d4135cf8d7ed8d
ger619 <abolger254@gmail.com>
2024-11-29 08:44:35 +0300

    Standardize quotes in image_modal_controller.js
    Converted double quotes to single quotes for consistency throughout the file. This change aims to improve the readability and maintainability of the code by ensuring a uniform coding style.

--- app/javascript/controllers/image_modal_controller.js
+++ app/javascript/controllers/image_modal_controller.js
@@ -1,26 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="image-modal"
 export default class extends Controller {
-  static targets = ["modal", "modalImage", "closeButton"]
+  static targets = ['modal', 'modalImage', 'closeButton']
 
   connect() {
-    this.showImage = this.showImage.bind(this)
-    document.addEventListener("click", this.showImage)
+    this.showImage = this.showImage.bind(this);
+    document.addEventListener('click', this.showImage);
   }
 
   disconnect() {
-    document.removeEventListener("click", this.showImage)
+    document.removeEventListener('click', this.showImage);
   }
 
   showImage(event) {
-    if (event.target.classList.contains("zoomable-image")) {
-      this.modalTarget.style.display = "block"
-      this.modalImageTarget.src = event.target.src
+    if (event.target.classList.contains('zoomable-image')) {
+      this.modalTarget.style.display = 'block';
+      this.modalImageTarget.src = event.target.src;
     }
   }
 
   closeModal() {
-    this.modalTarget.style.display = "none"
+    this.modalTarget.style.display = 'none';
   }
 }
