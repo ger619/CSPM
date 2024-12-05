979849ec96397f000d8c734272f051f8b1c59a46
ger619 <abolger254@gmail.com>
2024-12-05 12:19:03 +0300

    Merge pull request #302 from ger619/add-rating
    Make the rating stars

--- app/controllers/organisation_controller.rb
+++ app/controllers/organisation_controller.rb
@@ -1,10 +1,23 @@
 class OrganisationController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_organisation
+  before_action :set_organisation, only: %i[show edit update destroy]
   # before_action :set_user, only: %i[assign_user unassign_user]
 
   def index
-    @organisations = Organisation.all
+    @organisation = Organisation.all
+
+    @organisation = if params[:query].present?
+                      Organisation.where('name ILIKE ? OR description ILIKE ?',
+                               "%#{params[:query]}%", "%#{params[:query]}%")
+
+              else
+                Organisation.all.order('created_at DESC')
+              end
+
+    @per_page = 10
+    @page = (params[:page] || 1).to_i
+    @total_pages = (@organisation.count / @per_page.to_f).ceil
+    @organisation = @organisation.offset((@page - 1) * @per_page).limit(@per_page)
   end
 
   def show
@@ -42,7 +55,7 @@ class OrganisationController < ApplicationController
 
   def destroy
     @organisation.destroy
-    redirect_to organisations_path
+    redirect_to organisation_index_path
   end
 
   def assign_user
