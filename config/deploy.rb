64cf444ea58a62735832984c7698c3b54cdba431
ger619 <abolger254@gmail.com>
2024-12-10 11:09:32 +0300

    add cloudinary

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,3 +43,8 @@ set :keep_releases, 5
 # set :ssh_options, verify_host_key: :secure
 
 
+set :default_env, {
+  'CLOUDINARY_CLOUD_NAME' => 'dvplan8dp',
+  'CLOUDINARY_API_KEY' => 'y623452783817565',
+  'CLOUDINARY_API_SECRET' => 'WeHl8NTZLQGyKx8psN8ZsmLbyxUs'
+}
