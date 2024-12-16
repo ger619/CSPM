0987dbbda2deeb710e2bc41a6ca6c0e9af25788f
Robert Kanyoro <rmjesusaddict@gmail.com>
2024-12-16 15:26:13 +0300

    Fix the linters

--- app/models/update_history.rb
+++ app/models/update_history.rb
@@ -1,14 +1,14 @@
 class UpdateHistory < ApplicationRecord
-    belongs_to :ticket
-    belongs_to :user
+  belongs_to :ticket
+  belongs_to :user
 
-    # Optionally, you can define a method to capture changes
-    def self.record_update(ticket, user, change_details)
-        create!(
-        ticket: ticket,
-        user: user,
-        change_details: change_details,
-        updated_at: Time.current
-        )
-    end
+  # Optionally, you can define a method to capture changes
+  def self.record_update(ticket, user, change_details)
+    create!(
+      ticket: ticket,
+      user: user,
+      change_details: change_details,
+      updated_at: Time.current
+    )
+  end
 end
