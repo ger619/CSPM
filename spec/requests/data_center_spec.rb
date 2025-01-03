2e587e743557b332a05886a4a0bfe5a3b32d4339
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-01-03 05:35:50 +0300

    Fix the linter errors in the code

--- spec/requests/data_center_spec.rb
+++ spec/requests/data_center_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "DataCenters", type: :request do
-  describe "GET /index" do
+RSpec.describe 'DataCenters', type: :request do
+  describe 'GET /index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
