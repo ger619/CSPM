a26b5447c61220d8de133e569645cdb3803ebaaf
ger619 <abolger254@gmail.com>
2025-01-22 11:14:19 +0300

    QA Testing to be dne

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:client_id, role_ids: [])
+    params.require(:user).permit(:first_name, :last_name, :client_id, role_ids: [])
   end
 
   def filtered_user_params
