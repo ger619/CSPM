079ab6397e9c1e3c08052936307c225f16269800
ger619 <abolger254@gmail.com>
2025-01-27 19:41:56 +0300

    Adding changes to the data controllers

--- app/controllers/data_center_controller.rb
+++ app/controllers/data_center_controller.rb
@@ -1,7 +1,6 @@
 require 'csv'
 class DataCenterController < ApplicationController
   before_action :authenticate_user!
-
   def cease_fire_report
     authorize! :generate, :report # Check if the user can generate reports
 
