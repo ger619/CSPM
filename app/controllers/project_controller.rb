a9ef44beaadbd014e70718bad37ec3d302ca22fb
ger619 <abolger254@gmail.com>
2025-02-11 15:36:28 +0300

    Views on tickets to select

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -25,13 +25,9 @@ class ProjectController < ApplicationController
 
       # Apply filters if params are present
       @ticket = @ticket.where('tickets.created_at::date = ?', params[:created_at]) if params[:created_at].present?
-
       @ticket = @ticket.joins(:statuses).where(statuses: { name: params[:status] }) if params[:status].present?
-
       @ticket = @ticket.where(priority: params[:priority]) if params[:priority].present?
-
       @ticket = @ticket.where(issue: params[:issue]) if params[:issue].present?
-
       @ticket = @ticket.where(users: { id: params[:user_id] }) if params[:user_id].present?
 
       if params[:query].present?
@@ -49,13 +45,16 @@ class ProjectController < ApplicationController
 
       @tickets = if params[:filter] == 'closed_assigned'
                    @project.tickets.joins(:users, :statuses)
-                     .where(users: { id: current_user.id })
-                     .where(statuses: { name: %w[Closed Resolved] })
+                           .where(users: { id: current_user.id })
+                           .where(statuses: { name: %w[Closed Resolved] })
                  else
                    @project.tickets.joins(:users, :statuses)
-                     .where(users: { id: current_user.id })
-                     .where.not(statuses: { name: %w[Closed Resolved] })
+                           .where(users: { id: current_user.id })
+                           .where.not(statuses: { name: %w[Closed Resolved] })
                  end
+
+      # Order by descending creation date
+      @ticket = @ticket.order(created_at: :desc)
 
       # Pagination
       @per_page = 10
@@ -68,9 +67,9 @@ class ProjectController < ApplicationController
       @assigned_tickets_count = @project.tickets.joins(:users).where(users: { id: current_user.id }).count
 
       @closed_assigned_tickets = @project.tickets.joins(:users, :statuses)
-        .where(users: { id: current_user.id })
-        .where(statuses: { name: %w[Closed Resolved] })
-        .count
+                                         .where(users: { id: current_user.id })
+                                         .where(statuses: { name: %w[Closed Resolved] })
+                                         .count
       @breached_target_tickets_count = @project.tickets.count_target_breached_sla
     else
       redirect_to root_path, alert: 'You are not authorized to view this content.'
