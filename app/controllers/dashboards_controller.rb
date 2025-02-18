a054541ace3524d39441a9564a7c382d32f5cc9e
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-02-18 20:04:02 +0300

    Add the response initial response

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -12,13 +12,16 @@ class DashboardsController < ApplicationController
     software = Software.find_by(name: software_name)&.id
 
     if software
-      tickets_count = Ticket.where(software_id: software).count
       total_tickets_last_30_days = Ticket.where(software_id: software).where('created_at >= ?', 30.days.ago).count
+      breached_tickets_last_30_days = SlaTicket.joins(:ticket)
+        .where(tickets: { software_id: software })
+        .where('tickets.created_at >= ?', 30.days.ago)
+        .where(sla_status: 'Breached')
+        .count
 
       stats = {
-        users_count: User.count,
-        tickets_count: tickets_count,
-        total_tickets_last_30_days: total_tickets_last_30_days
+        total_tickets_last_30_days: total_tickets_last_30_days,
+        breached_tickets_last_30_days: breached_tickets_last_30_days
       }
 
       render json: stats
@@ -33,8 +36,6 @@ class DashboardsController < ApplicationController
     # Default statistics, assuming 'BREFT' is the default software
     br_islamic_software_id = Software.find_by(name: 'BREFT')&.id
     {
-      users_count: User.count,
-      tickets_count: Ticket.count,
       br_islamic_tickets_count: Ticket.where(software_id: br_islamic_software_id).count
     }
   end
