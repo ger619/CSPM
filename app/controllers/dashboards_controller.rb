bef6996b8c4c8fd78c2940e234ec4248c6cefffb
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-02-24 13:17:26 +0300

    Hotfix the number of days for tcikets created

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -12,62 +12,62 @@ class DashboardsController < ApplicationController
     software = Software.find_by(name: software_name)&.id
 
     if software
-      total_tickets_last_30_days = Ticket.where(software_id: software).where('created_at >= ?', 300.days.ago).count
+      total_tickets_last_30_days = Ticket.where(software_id: software).where('created_at >= ?', 30.days.ago).count
       breached_tickets_last_30_days = SlaTicket.joins(:ticket)
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_status: 'Breached')
         .count
 
       not_breached_tickets_last_30_days = SlaTicket.joins(:ticket)
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_status: 'Not Breached')
         .count
 
       response_breached_tickets_last_30_days = SlaTicket.joins(:ticket)
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_target_response_deadline: 'Breached')
         .count
 
       not_response_breached_tickets_last_30_days = SlaTicket.joins(:ticket)
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_target_response_deadline: 'Not Breached')
         .count
 
       resolution_breached_tickets_last_30_days = SlaTicket.joins(:ticket)
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_resolution_deadline: 'Breached')
         .count
 
       not_resolution_breached_tickets_last_30_days = SlaTicket.joins(:ticket)
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_resolution_deadline: 'Not Breached')
         .count
 
       pending_resolution_30_days = Ticket.where(software_id: software)
-        .where('created_at >= ?', 300.days.ago)
+        .where('created_at >= ?', 30.days.ago)
         .where(id: SlaTicket.where(sla_resolution_deadline: nil).select(:ticket_id))
         .count
 
       pending_initial_response_30_days = Ticket.where(software_id: software)
-        .where('created_at >= ?', 300.days.ago)
+        .where('created_at >= ?', 30.days.ago)
         .where(id: SlaTicket.where(sla_status: nil).select(:ticket_id))
         .count
 
       pending_target_response_30_days = Ticket.where(software_id: software)
-        .where('created_at >= ?', 300.days.ago)
+        .where('created_at >= ?', 30.days.ago)
         .where(id: SlaTicket.where(sla_target_response_deadline: nil).select(:ticket_id))
         .count
 
       breached_tickets_per_assignee = SlaTicket
         .joins(ticket: { taggings: :user }) # Ensures proper joins
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_target_response_deadline: 'Breached')
         .group('users.first_name', 'users.last_name')
         .count
@@ -75,7 +75,7 @@ class DashboardsController < ApplicationController
       breached_resolution_tickets_per_project = SlaTicket
         .joins(ticket: :project) # Join the projects table
         .where(tickets: { software_id: software })
-        .where('tickets.created_at >= ?', 300.days.ago)
+        .where('tickets.created_at >= ?', 30.days.ago)
         .where(sla_target_response_deadline: 'Breached')
         .group('projects.title') # Group by project title
         .count
