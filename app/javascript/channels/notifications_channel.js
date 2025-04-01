fd9896899e6c4b2edfb491b1f6bb8605f1ebb77f
ger619 <abolger254@gmail.com>
2025-04-01 17:43:34 +0300

    Refactor JavaScript imports and improve code formatting in consumer and notifications_channel files

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -1,6 +1,6 @@
-import consumer from "./consumer"
+import consumer from './consumer';
 
-consumer.subscriptions.create("NotificationsChannel", {
+consumer.subscriptions.create('NotificationsChannel', {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -8,11 +8,11 @@ consumer.subscriptions.create("NotificationsChannel", {
   disconnected() {
     // Called when the subscription has been terminated by the server
   },
-
+  // eslint-disable-next-line no-unused-vars
   received(data) {
-    const notificationButton = document.getElementById("dropdownNotificationButton");
+    const notificationButton = document.getElementById('dropdownNotificationButton');
     if (notificationButton) {
-      notificationButton.innerHTML += `<div class="absolute block w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-0.5 start-2.9 dark:border-gray-900"></div>`;
+      notificationButton.innerHTML += '<div class="absolute block w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-0.5 start-2.9 dark:border-gray-900"></div>';
     }
-  }
+  },
 });
