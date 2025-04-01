96319cd36f1d89cf9c6992a7e08dfb2c88cfd393
@ger619 <abolger254@gmail.com>
2025-04-01 19:30:29 +0300

    Refactor form layout in _form.html.erb to improve structure

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
     resources :tickets do
       collection do
         get 'all_tickets'
+        get 'index_home'
       end
       member do
         post :assign_tag
