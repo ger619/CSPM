05f90763b21120e203ce6cae62b0f3cc3fcf9aad
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-04-02 09:18:21 +0300

    Remove trailing space causing linter errors

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,5 +1,5 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features 
+// You can generate new channels where WebSocket features
 // live using the `bin/rails generate channel` command.
 
 // eslint-disable-next-line import/no-unresolved
