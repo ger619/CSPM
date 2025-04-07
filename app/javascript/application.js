3f2733286a9c6d80e3748d8a732503d1f3bb0b35
@ger619 <abolger254@gmail.com>
2025-04-07 12:41:10 +0300

    Remove commented imports for clarity and maintainability

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,11 @@ import '@hotwired/turbo-rails';
 import 'controllers';
 import 'trix';
 import '@rails/actiontext';
-// import "./controllers/trix_custom_formatting"
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 //= require 'chartkick'
 //= require 'chart.js'
 // import "./channels"
+// import "./controllers/trix_custom_formatting"
+
