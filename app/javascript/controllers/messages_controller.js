6ac333673e48b4ef081df2372a75ee4f52f906a8
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-01 17:17:06 +0300

    The tickets messagesright side

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -3,8 +3,10 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ["form"];
 
-  toggleForm() {
+  toggleForm(event) {
+    event.preventDefault();
     this.formTarget.classList.toggle('hidden');
+    
     if (!this.formTarget.classList.contains('hidden')) {
       this.formTarget.scrollIntoView({ behavior: 'smooth' });
     }
