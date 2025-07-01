e34ba04eafb53ebbcd2201b580049cefe1c16c66
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-01 17:21:59 +0300

    Fix eslint errors

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -1,12 +1,12 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ["form"];
+  static targets = ['form'];
 
   toggleForm(event) {
     event.preventDefault();
     this.formTarget.classList.toggle('hidden');
-    
+
     if (!this.formTarget.classList.contains('hidden')) {
       this.formTarget.scrollIntoView({ behavior: 'smooth' });
     }
