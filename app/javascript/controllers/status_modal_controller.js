5ca2351d2b741dc0e77ff5ac2b75b41f7a57b373
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-01 11:04:13 +0300

    Update included:   - Add the update ticket assigned modal   - Update the status modal JS controller code to include functionality for opening the popp modal   - Update the assign people tickets partial to include the updated code for the assign people modal

--- app/javascript/controllers/status_modal_controller.js
+++ app/javascript/controllers/status_modal_controller.js
@@ -1,27 +1,51 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['dropdown']
+  static targets = ['dropdown', 'modal']
 
-  toggle() {
+  connect() {
+    document.addEventListener('keydown', this.closeWithKey.bind(this));
+    document.addEventListener('click', this.closeOnClickOutside.bind(this));
+  }
+
+  disconnect() {
+    document.removeEventListener('keydown', this.closeWithKey.bind(this));
+    document.removeEventListener('click', this.closeOnClickOutside.bind(this));
+  }
+
+  // Status dropdown methods
+  toggleDropdown() {
     this.dropdownTarget.classList.toggle('hidden');
   }
 
-  close() {
+  closeDropdown() {
     this.dropdownTarget.classList.add('hidden');
+  }
+
+  // Modal methods
+  openModal() {
+    this.modalTarget.classList.remove('hidden');
+    document.body.classList.add('overflow-hidden');
+  }
+
+  closeModal() {
+    this.modalTarget.classList.add('hidden');
+    document.body.classList.add('overflow-auto');
   }
 
   // Close when clicking outside
   closeOnClickOutside(event) {
-    if (!this.element.contains(event.target)) {
-      this.close();
+    if (!this.element.contains(event.target) && !this.modalTarget.contains(event.target)) {
+      this.closeDropdown();
+      this.closeModal();
     }
   }
 
   // Close when pressing Escape
   closeWithKey(e) {
     if (e.key === 'Escape') {
-      this.close();
+      this.closeDropdown();
+      this.closeModal();
     }
   }
 }
