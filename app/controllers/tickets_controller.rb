3732e67dd160099809e390ff7d33dcc912f25142
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-08 14:47:20 +0300

    Update included:   - Add the @page pagination for the non breached SLA tickets   - Ensure thta the non breached SLA tickets are using the same partials

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -460,6 +460,10 @@ class TicketsController < ApplicationController
   def non_breached_sla_tickets
     @project = Project.find(params[:project_id])
     @tickets = @project.tickets.joins(:sla_tickets).where("sla_tickets.sla_status = 'Not Breached'")
+
+    @per_page = 10
+    @page = (params[:page] || 1).to_i
+    @total_pages = (@tickets.count / @per_page.to_f).ceil
   end
 
   def modal_show
