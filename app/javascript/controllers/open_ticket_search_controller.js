309a8dcb2f38b30b01787e3d4f985443e2de912d
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-08 10:33:10 +0300

    Update included:   - Created a stimulus controller for the open ticket search file(open_ticket_search_controller.js)   - Update the Open ticket search (tickets/_open_ticket_search.html.erb) removing the inline JS and adding the Stimulus data attributes

---
+++ app/javascript/controllers/open_ticket_search_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["alert"]
+
+  connect() {
+    // Controller is now connected to the alert element
+  }
+
+  closeAlert() {
+    this.alertTarget.remove() // Completely removes the modal from the DOM
+  }
+}
