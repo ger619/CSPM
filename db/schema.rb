1e989ad3af816fa293fdc5a7c9e57c117dec981b
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-15 10:55:40 +0300

    Add a status column in the product table and migrating it to the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_14_082922) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_15_074454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -280,6 +280,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_14_082922) do
     t.uuid "script_id"
     t.text "document_name"
     t.string "budget"
+    t.string "status", default: "draft"
     t.index ["client_id"], name: "index_products_on_client_id"
     t.index ["groupware_id"], name: "index_products_on_groupware_id"
     t.index ["name"], name: "index_products_on_name", unique: true
