112cd768522b20bd4690d4b3a65f24d719a6ba4d
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-21 10:21:56 +0300

    Run migration to rmeove the topic column from the tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_21_060903) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_21_071715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -443,7 +443,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_21_060903) do
 
   create_table "tasks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
-    t.string "topic"
     t.string "description"
     t.uuid "product_id", null: false
     t.uuid "user_id", null: false
