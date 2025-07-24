b427027cb3437572e2ba0d622f619c2c03f8e7ef
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-07-24 10:37:09 +0300

    Check and fix all found Rubocop offences

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -13,26 +13,26 @@ class ProductController < ApplicationController
 
       @product = @product
         .joins("LEFT JOIN action_text_rich_texts ON action_text_rich_texts.record_id = products.id AND action_text_rich_texts.record_type = 'Product'")
-        .joins("LEFT JOIN products_statuses ON products_statuses.product_id = products.id")
-        .joins("LEFT JOIN statuses ON statuses.id = products_statuses.status_id")
+        .joins('LEFT JOIN products_statuses ON products_statuses.product_id = products.id')
+        .joins('LEFT JOIN statuses ON statuses.id = products_statuses.status_id')
         .where(
-          "products.document_name ILIKE :q OR 
-          statuses.name ILIKE :q OR 
+          "products.document_name ILIKE :q OR
+          statuses.name ILIKE :q OR
           products.budget ILIKE :q OR
           action_text_rich_texts.body ILIKE :q OR
           EXISTS (
-            SELECT 1 FROM clients 
-            WHERE clients.id = products.client_id 
+            SELECT 1 FROM clients
+            WHERE clients.id = products.client_id
             AND clients.name ILIKE :q
           ) OR
           EXISTS (
-            SELECT 1 FROM softwares 
-            WHERE softwares.id = products.software_id 
+            SELECT 1 FROM softwares
+            WHERE softwares.id = products.software_id
             AND softwares.name ILIKE :q
           ) OR
           EXISTS (
-            SELECT 1 FROM groupwares 
-            WHERE groupwares.id = products.groupware_id 
+            SELECT 1 FROM groupwares
+            WHERE groupwares.id = products.groupware_id
             AND groupwares.name ILIKE :q
           )",
           q: search_query
