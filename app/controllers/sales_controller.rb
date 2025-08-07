aba45793971c47d9b9c9e4d3e8a2777c0bdca364
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-07 11:59:04 +0300

    Update included:   - In sidebar view, show the Sales only for Admin   - in Sales controller load the product product resource   - in the product index partial topbar, show the create product button only for admin as well   - In the sales index view, show the topbar that has the create product button

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,6 +2,8 @@ class SalesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @product = Product.includes(softwares: :groupwares)
+    @used_statuses = @product.map { |p| p.statuses.first&.name }.compact.uniq
   end
 
 end
