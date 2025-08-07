d9404df5c0f26d4a39db5bb6e21b97a931dfab3d
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-07 10:12:30 +0300

    Update included:   - Add migration adding the submodule field to the defects table   - Run the migrations to update the project schema   - Update the defectsnew form to add the submodule to the records

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_30_093856) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_07_070616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -175,6 +175,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_30_093856) do
     t.datetime "updated_at", null: false
     t.uuid "product_id", null: false
     t.uuid "user_id", null: false
+    t.string "submodule"
     t.index ["product_id"], name: "index_defects_on_product_id"
     t.index ["user_id"], name: "index_defects_on_user_id"
   end
