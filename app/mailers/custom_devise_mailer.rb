55a6aeeb11095fb45a908611f08e7e73ab870d09
ger619 <abolger254@gmail.com>
2026-08-11 22:57:23 +0300

    Remove total list section and related chart data logic from home controller

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,6 +1,7 @@
 class CustomDeviseMailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
+
   default template_path: 'devise/mailer'
 
   def invitation_instructions(record, token, opts = {})
