d0a2503a6c0c6cf3405996995ea39f608874ca81
ger619 <abolger254@gmail.com>
2026-08-12 15:02:40 +0300

    Remove total list section and related chart data logic from home controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,15 +20,30 @@ Role.find_or_create_by!(name: 'sales')
 Role.find_or_create_by!(name: 'ceo')
 Role.find_or_create_by!(name: 'hod')
 
-a = User.create!(email: 'admin@craftsilicon.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Jay', last_name: 'Admin')
+a = User.create!(email: 'admin@greatercare.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Jay', last_name: 'Admin')
 a.add_role(:admin)
 
-b = User.create!(email: 'project@craftsilicon.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Project', last_name: 'Manager')
+b = User.create!(email: 'project@greatercare.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Project', last_name: 'Manager')
 b.add_role('project manager')
 
-c = User.create!(email: 'agent@craftsilicon.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Agent', last_name: 'Active')
+c = User.create!(email: 'agent@greatercare.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Agent', last_name: 'Active')
 c.add_role(:agent)
 
-d = User.create!(email: 'client@craftsilicon.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Client', last_name: 'Active')
+d = User.create!(email: 'client@greatercare.com', password: 'password', confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now, first_name: 'Client', last_name: 'Active')
 d.add_role(:client)
+admin = User.find_by!(email: 'admin@greatercare.com')
 
+
+
+Client.create!(name: 'Greater Care', email: 'client@greatercare.com', user_id: User.find_by(email: 'admin@greatercare.com').id)
+
+Software.create!(name: 'Greater Care', user_id: User.find_by(email: 'project@greatercare.com').id)
+Groupware.create!(name: 'Greater Care', software_id: Software.find_by(name: 'Greater Care').id, user_id: User.find_by(email: 'admin@greatercare.com').id)
+
+statuses = %w[Decline Accept Pending Approved Assigned On-Hold Completed In-Progress Rejected Cancelled Closed]
+
+statuses.each do |status_name|
+  Status.find_or_create_by!(name: status_name) do |status|
+    status.user_id = admin.id
+  end
+end
